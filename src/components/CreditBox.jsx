9a988a99aef1e4bb30215f5d4ebd74dd56c9ce63
andriuslau <andriausl@gmail.com>
2021-11-14 20:30:58 +0200

    Added some changes

--- src/components/CreditBox.jsx
+++ src/components/CreditBox.jsx
@@ -1,5 +1,5 @@
 export const CreditBox = () => (
   <address className="my-2 fs-6 text-white">
-    <pre>Created by <a href="http://juniorjavadeveloper.com" className="text-reset text-decoration-underline" rel="author">JuniorJavaDeveloper.com</a> using <a href="https://github.com/harmonify" className="text-reset text-decoration-underline" rel="author">Wendy</a> template</pre>
+    <pre>Created by <a href="http://juniorjavadeveloper.com" className="text-reset text-decoration-underline" rel="author">JJD</a> using <a href="https://github.com/harmonify" className="text-reset text-decoration-underline" rel="author">Wendy</a> template</pre>
   </address>
 );
